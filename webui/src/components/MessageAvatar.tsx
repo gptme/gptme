9bd3657d2b3f9babba18f71f178a2a7dca453e7e
Erik Bjäreholt <erik@bjareho.lt>
2026-02-12 14:54:12 +0100

    fix(webui): show avatar tooltip below instead of to the side

--- webui/src/components/MessageAvatar.tsx
+++ webui/src/components/MessageAvatar.tsx
@@ -98,7 +98,7 @@ export function MessageAvatar({
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>{avatarElement}</TooltipTrigger>
-        <TooltipContent side={isUser ? 'left' : 'right'}>{tooltipText}</TooltipContent>
+        <TooltipContent side="bottom">{tooltipText}</TooltipContent>
       </Tooltip>
     </TooltipProvider>
   );
