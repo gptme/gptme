4bef84f82ae282863e05515dc8665fd38f6fe114
Bob <bob@superuserlabs.org>
2026-06-29 22:44:12 +0000

    refactor(test): move itertools import to top level

--- tests/test_tools_computer.py
+++ tests/test_tools_computer.py
@@ -1,5 +1,6 @@
 """Tests for the computer tool."""
 
+import itertools
 import shutil
 import subprocess
 from typing import Any, cast
@@ -1186,8 +1187,6 @@ def test_wait_for_change_polls_with_backoff(mock_transport, mock_res, tmp_path):
     # Expire the loop on the 4th poll so we get 3 sleep calls and see the backoff
     # Use an infinite tail (repeat(100.0)) to avoid StopIteration inside a generator
     # (PEP 479: StopIteration raised in a generator becomes RuntimeError)
-    import itertools
-
     monotonic_iter = itertools.chain([0.0, 0.0, 0.0, 0.0], itertools.repeat(100.0))
 
     with (
