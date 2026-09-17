91b92454f6bfb04f272592e72477d607a4a31d5f
Bob <bob@superuserlabs.org>
2026-09-17 20:23:33 +0000

    test(webui): cap jest maxWorkers at 4 to prevent container thrash
    Git-Session-Id: 8497

--- webui/jest.config.ts
+++ webui/jest.config.ts
@@ -1,4 +1,6 @@
 export default {
+  // Cap workers to prevent container thrash on shared hosts (mirrors vitest maxWorkers)
+  maxWorkers: 4,
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
